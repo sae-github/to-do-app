095ea4e3f2d7892edc88eb638c3ef5983268c56f
sae-github <miyasae1126@gmail.com>
2022-09-22 20:15:11 +0900

    feat: toggle dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { ToDoForm } from "./components/ToDoForm";
 import { ToDoList } from "./components/ToDoList";
+const theme = localStorage.getItem("theme");
 
 export const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(Boolean(theme));
+  const toggleDarkMode = () => {
+    setIsDarkMode((mode) => !mode);
+  };
+
+  useEffect(() => {
+    if (
+      isDarkMode ||
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.removeItem("theme");
+    }
+  }, [isDarkMode]);
+
   return (
-    <>
-      <Header />
+    <div>
+      <Header toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
       <div className="max-w-4xl mx-auto mt-9 px-[20px]">
         <ToDoForm />
         <ToDoList />
       </div>
-    </>
+    </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,28 @@
-export const Header = () => {
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+
+interface DarkMode {
+  toggleDarkMode: () => void;
+  isDarkMode: boolean;
+}
+
+export const Header = ({ toggleDarkMode, isDarkMode }: DarkMode) => {
   return (
-    <header className="bg-[#3aa7a0] text-white p-3 text-center">
-      <h1 className="text-3xl font-en">React TODO App</h1>
+    <header className="bg-[#3aa7a0] text-white p-3 text-center dark:bg-[#0F172A]">
+      <div className="max-w-4xl mx-auto px-[20px] relative">
+        <h1 className="text-3xl font-en">React TODO App</h1>
+        {isDarkMode ? (
+          <Brightness7Icon
+            className="cursor-pointer hover:opacity-75 absolute right-0 top-1/2 translate-y-[-50%]"
+            onClick={toggleDarkMode}
+          />
+        ) : (
+          <Brightness4Icon
+            className="cursor-pointer hover:opacity-75 absolute right-0 top-1/2 translate-y-[-50%]"
+            onClick={toggleDarkMode}
+          />
+        )}
+      </div>
     </header>
   );
 };
