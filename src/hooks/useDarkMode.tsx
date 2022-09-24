cd4508b47b0d26d63949b72ac46da0040171a73f
sae-github <miyasae1126@gmail.com>
2022-09-24 09:07:05 +0900

    refactor: fix processing when os is set to dark mode

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -3,22 +3,30 @@ const osDark = window.matchMedia("(prefers-color-scheme: dark)");
 const theme = localStorage.getItem("theme");
 
 const onDarkMode = () => {
+  console.log("ON Dark");
   document.documentElement.classList.add("dark");
   localStorage.setItem("theme", "dark");
 };
 
 const offDarkMode = () => {
+  console.log("OFF Dark");
   document.documentElement.classList.remove("dark");
   localStorage.removeItem("theme");
 };
 
+osDark.addEventListener("change", (event) => {
+  event.matches ? onDarkMode() : offDarkMode();
+});
+
 export const useDarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useState(Boolean(theme));
+  const [isDarkMode, setIsDarkMode] = useState(
+    Boolean(theme) || osDark.matches
+  );
 
   const toggleDarkMode = () => setIsDarkMode((mode) => !mode);
 
   useEffect(() => {
-    isDarkMode || osDark.matches ? onDarkMode() : offDarkMode();
+    isDarkMode ? onDarkMode() : offDarkMode();
   }, [isDarkMode]);
 
   return { isDarkMode, toggleDarkMode };
