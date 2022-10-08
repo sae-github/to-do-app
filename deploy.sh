62790c09709c4f1833e83d8b5e970b56b91a329a
sae-github <miyasae1126@gmail.com>
2022-10-08 22:58:40 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# エラー時は停止
+set -e
+
+# ビルド
+npm run build
+
+# ビルド出力ディレクトリに移動
+cd dist
+
+# カスタムドメインにデプロイする場合
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# https://<USERNAME>.github.io にデプロイする場合
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# https://<USERNAME>.github.io/<REPO> にデプロイする場合
+git push -f git@github.com:sae-github/to-do-app master:gh-pages
+
+cd -
